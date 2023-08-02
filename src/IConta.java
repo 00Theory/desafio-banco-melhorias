2099d572c7cec4ae126c54afe6e92cbe6c6fb08e
00Theory <marco200002010@outlook.com>
2023-08-02 10:34:56 -0300

    Inserção dos códigos do projeto de referência

--- src/IConta.java
+++ src/IConta.java
@@ -1,3 +1,9 @@
-public class IConta {
-    
+public interface IConta {
+    void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
 }
